0262e7a681632797555195c3296384aa0e9344cf
salehiin <salehin.ay@gmail.com>
2024-09-17 21:54:07 +0600

    m3 map, forEach filter, find

---
+++ m1/js-concepts-for-react/array-methods.js
@@ -0,0 +1,32 @@
+const products = [
+    {name: 'laptop', price: 32000, brand: 'lenovo', color: 'silver'},
+    {name: 'phone', price: 7000, brand: 'iphone', color: 'golden'},
+    {name: 'watch', price: 3000, brand: 'casio', color: 'yellow'},
+    {name: 'sunglass', price: 300, brand: 'ribon', color: 'black'},
+    {name: 'camera', price: 9000, brand: 'canon', color: 'silver'},
+    // {name: 'laptop', price: 32000, brand: 'canon', color: 'gray'},
+]
+const brands = products.map(product => product.brand);
+// console.log(brands);
+const prices = products.map(product => product.price);
+// console.log(prices);
+
+// 2. forEach
+products.forEach(product => console.log(product))
+products.forEach(product => console.log(product.color))
+
+
+products.forEach(product => {
+
+})
+
+// 3. filter
+const cheap = products.filter(product => product.price <= 5000);
+console.log(cheap);
+
+const specificName = products.filter(product => product.name.includes('n'))
+console.log(specificName)
+
+// 4. find
+const special = products.find(product => product.name.includes('n'));
+console.log(special)
